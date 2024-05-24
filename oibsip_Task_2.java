2d1df9481270715233ab95dfe4875f708c17e408
Dhrumil Asodiya <152181508+weirdo764@users.noreply.github.com>
2024-05-24 15:00:33 +0530

    Update oibsip_Task_2.java

--- oibsip_Task_2.java
+++ oibsip_Task_2.java
@@ -1,15 +1,14 @@
 import java.util.*;
-
-
 class Game{
     public static int score = 0;
-    public int randomNumber;
+    public static int randomNumber;
     public int userInput;
     public int guessNo = 0;
-    Game(){
+    Game(int n){
         Random r = new Random();
         randomNumber = r.nextInt(100);
-        System.out.println("the generated Number is "+randomNumber);
+        System.out.println("You have "+n+" attempts to guess the number");
+//        System.out.println("the generated Number is "+randomNumber);
     }
 
     void takeUserInput(){
@@ -17,53 +16,62 @@ class Game{
         System.out.println("Enter your Number :");
         Scanner sc = new Scanner(System.in);
         userInput = sc.nextInt();
-
     }
 
     boolean isCorrectNumber()
     {
         guessNo++;
         if (userInput == randomNumber) {
-            System.out.println("You Guessed is Correct "+userInput);
+            System.out.println("You Guessed the Correct number: "+userInput);
             System.out.println("You completed Game in "+guessNo+" Guess");
-            score++;
+            if(guessNo >=15) {
+                score++;
+            } else if (guessNo>=10) {
+                score = score+5;
+            }
+            else if (guessNo>=5){
+                score = score+10;
+            }
+            else if (guessNo>1) {
+                score = score+11;
+            }
             return false;
         }
         else if (userInput > randomNumber) {
-            System.out.println("Enter Smaller Number");
+            System.out.println("Enter Smaller Number\n");
         }
         else if (userInput < randomNumber) {
-            System.out.println("Enter Larger Number");
+            System.out.println("Enter Larger Number\n");
         }
         return true;
     }
-
 }
 
 public class oibsip_Task_2{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(true) {
-            Game g1 = new Game();
-            boolean b = true;
             int attempts = 0;
+            System.out.println("How may attempts you want?");
+            int lastAttempt = sc.nextInt();
+            Game g1 = new Game(lastAttempt);
+            boolean b = true;
             do {
                 g1.takeUserInput();
                 b = g1.isCorrectNumber();
                 attempts++;
-                System.out.println("You have "+(5-attempts)+" attempts left");
-                if (attempts == 5 || !b){
+                if (attempts == lastAttempt){
+                    System.out.println("You have 0 attempts left\nYou Lost");
                     break;
                 }
             }while(b);
-            System.out.println("Do you want to play again?");
+            System.out.println("\nDo you want to play again? Y/N");
+            sc.nextLine();
             String res = sc.nextLine();
             if (res.equalsIgnoreCase("N")) {
                 break;
             }
         }
-
-
         System.out.println("Your Final Score is "+ Game.score);
     }
 }
